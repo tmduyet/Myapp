01ca764b43ec80b0c481c1880a381394e28df57c
conga123 <tmduyet@gmail.com>
2020-08-15 00:12:45 +0700

    fix2

--- app/src/main/java/com/example/myapp/AddFragment.java
+++ app/src/main/java/com/example/myapp/AddFragment.java
@@ -1,12 +1,24 @@
 package com.example.myapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +26,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class AddFragment extends Fragment {
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +40,17 @@ public class AddFragment extends Fragment {
     public AddFragment() {
         // Required empty public constructor
     }
+
+    private Button btngallery;
+    private ImageView imageView;
+    private static final int IMAGE_PICK_CODE = 102;
+    private static final int PERMISSION_CODE = 103;
+    void init(View view)
+    {
+        btngallery = (Button) view.findViewById(R.id.btnchonanh);
+        imageView = (ImageView) view.findViewById(R.id.imgsach);
+    }
+
 
     /**
      * Use this factory method to create a new instance of
@@ -49,9 +73,53 @@ public class AddFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
+
+
+
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    private void Pickimage()
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE)
+        {
+            imageView.setImageURI(data.getData());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case PERMISSION_CODE:
+            {
+                if (grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+                {
+                    Pickimage();
+                }
+                else{
+                    Toast.makeText(getContext(), "Yêu cầu bị từ chối", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
     }
 
@@ -59,6 +127,31 @@ public class AddFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View v =  inflater.inflate(R.layout.fragment_add, container, false);
+
+        init(v);
+        btngallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                {
+                    if(ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED)
+                    {
+                        String[] permisson = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permisson, PERMISSION_CODE);
+                    }
+                    else
+                        {
+                             Pickimage();
+                        }
+
+                }
+                else
+                {
+                    Pickimage();
+                }
+            }
+        });
+        return v;
     }
 }
