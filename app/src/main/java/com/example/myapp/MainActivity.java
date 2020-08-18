5e95aa8764624056ca52e2e2df18c96aefc7f8e5
conga123 <tmduyet@gmail.com>
2020-08-18 21:42:34 +0700

    importdatabase3

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         Init();
+
         setFragment(homeFragment);
         main_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
